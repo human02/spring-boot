466e3993ca2d8c9d8556d77684eeb42c6f69bbe7
Aarav Chandra <aaravchandra02@gmail.com>
2021-04-01 16:25:30 -0700

    first commit

---
+++ Learning/one/src/main/java/com/aarav/one/testController.java
@@ -0,0 +1,14 @@
+package com.aarav.one;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class testController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String handleTest() {
+        return "This is a successful test";
+    }
+}
