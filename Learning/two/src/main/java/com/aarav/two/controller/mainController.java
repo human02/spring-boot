4db8ce64b25afa68fe93181b0365168945bfa89f
Aarav Chandra <aaravchandra02@gmail.com>
2021-04-01 19:09:03 -0700

    mapping completed

---
+++ Learning/two/src/main/java/com/aarav/two/controller/mainController.java
@@ -0,0 +1,13 @@
+package com.aarav.two.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("This is home page");
+        return "home";
+    }
+}
