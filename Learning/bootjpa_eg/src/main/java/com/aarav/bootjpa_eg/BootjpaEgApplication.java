6ccc6602e347ca455895a07486a0c89b03118da2
Aarav Chandra <aaravchandra02@gmail.com>
2021-04-01 22:27:29 -0700

    initial commit

---
+++ Learning/bootjpa_eg/src/main/java/com/aarav/bootjpa_eg/BootjpaEgApplication.java
@@ -0,0 +1,13 @@
+package com.aarav.bootjpa_eg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpaEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpaEgApplication.class, args);
+	}
+
+}
---
+++ Learning/bootjpa_eg/src/test/java/com/aarav/bootjpa_eg/BootjpaEgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarav.bootjpa_eg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjpaEgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
