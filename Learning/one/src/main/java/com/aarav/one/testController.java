f74eec154bc971c3c96e624e6b8d78f8f309f7fa
Aarav Chandra <aaravchandra02@gmail.com>
2021-04-01 17:36:21 -0700

    added comment

--- Learning/one/src/main/java/com/aarav/one/testController.java
+++ Learning/one/src/main/java/com/aarav/one/testController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class testController {
     @RequestMapping("/test")
-    @ResponseBody
+    @ResponseBody // This is required to return the string as an HTML.
     public String handleTest() {
         return "This is a successful test";
     }
