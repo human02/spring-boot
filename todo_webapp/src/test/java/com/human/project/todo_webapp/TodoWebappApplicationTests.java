23be090a2e33a222b52ae5c98ffa66aa2c2fbded
Surya-Dev <aaravchandra02@gmail.com>
2023-02-04 00:13:52 -0800

    init

---
+++ todo_webapp/src/test/java/com/human/project/todo_webapp/TodoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.project.todo_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
