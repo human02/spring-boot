e96130afb4156bc8a0374ba17610069f44d7ccce
Surya-Dev <aaravchandra02@gmail.com>
2023-02-04 00:26:43 -0800

    add HTML response

--- todo_webapp/src/main/java/com/human/project/todo_webapp/hello/HelloWorldController.java
+++ todo_webapp/src/main/java/com/human/project/todo_webapp/hello/HelloWorldController.java
@@ -13,4 +13,21 @@ public class HelloWorldController {
 		return "Hello! What are you learning today?\n";
 	}
 
+	
+	@RequestMapping("/say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+		
+	}
 }
