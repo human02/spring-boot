0f27b6919001faf4526c9f72182a4a88e0f950ee
Surya-Dev <aaravchandra02@gmail.com>
2023-02-07 15:17:03 -0800

    add todo controller

---
+++ todo_webapp/src/main/java/com/human/project/todo_webapp/todo/ToDoController.java
@@ -0,0 +1,28 @@
+package com.human.project.todo_webapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ToDoController {
+	
+	public ToDoController(ToDoService todoService) {
+		super();
+		this.todoService = todoService;
+	}
+
+	private ToDoService todoService;
+		
+	
+	@RequestMapping("list")
+	public String listAllTodos(ModelMap model) {
+		List<ToDo> todos = todoService.findByUser("Aarav");
+		model.addAttribute("todos", todos);
+		
+		return "todoPage";
+	}
+
+}
