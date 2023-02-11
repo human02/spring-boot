9fb61cb8cdff31f21fad0aa449d7377ba1c7fbb8
Surya-Dev <aaravchandra02@gmail.com>
2023-02-11 00:37:45 -0800

    rename bean to solve error

--- todo_webapp/src/main/java/com/human/project/todo_webapp/todo/ToDoController.java
+++ todo_webapp/src/main/java/com/human/project/todo_webapp/todo/ToDoController.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -23,7 +25,7 @@ public class ToDoController {
 
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
-		List<ToDo> todos = todoService.findByUser("test");
+		List<Todo> todos = todoService.findByUser("test");
 		model.addAttribute("todos", todos);
 
 		return "todoPage";
@@ -31,15 +33,19 @@ public class ToDoController {
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewToDoPage(ModelMap model) {
-		String username = (String)model.getAttribute("name");
-		ToDo todo = new ToDo(0,username,"",LocalDate.now().plusYears(1),false);
+		String username = (String) model.getAttribute("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
 		model.put("todo", todo);
 		return "todo";
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewToDo(ModelMap model, ToDo todo) {
-		String username = (String)model.getAttribute("name");
+	public String addNewToDo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "todo";
+		}
+		String username = (String) model.getAttribute("name");
 		todoService.addToDo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos"; // redirect: redirects to a previous request mapping as mentioned in our case
 	}
--- todo_webapp/src/main/java/com/human/project/todo_webapp/todo/ToDoService.java
+++ todo_webapp/src/main/java/com/human/project/todo_webapp/todo/ToDoService.java
@@ -9,21 +9,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class ToDoService {
 
-	private static List<ToDo> todos = new ArrayList<ToDo>();
+	private static List<Todo> todos = new ArrayList<Todo>();
 	 public static int idCount=0;
 	
 	static {
-		todos.add(new ToDo(++idCount, "test", "Learn AWS", LocalDate.now().plusYears(1), false));
-		todos.add(new ToDo(++idCount, "test", "Learn Rust", LocalDate.now().plusYears(2), false));
-		todos.add(new ToDo(++idCount, "test", "Learn Kafka", LocalDate.now().plusYears(3), false));
+		todos.add(new Todo(++idCount, "test", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++idCount, "test", "Learn Rust", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++idCount, "test", "Learn Kafka", LocalDate.now().plusYears(3), false));
 	}
 
-	public List<ToDo> findByUser(String user) {
+	public List<Todo> findByUser(String user) {
 //		System.out.println("CHECK IT "+LocalDate.now().plusYears(1));
 		return todos;
 	}
 	
 	public void addToDo(String user,String description,LocalDate targetdate,boolean done) {
-		todos.add(new ToDo(++idCount,user,description,targetdate,done));
+		todos.add(new Todo(++idCount,user,description,targetdate,done));
 	}
 }
